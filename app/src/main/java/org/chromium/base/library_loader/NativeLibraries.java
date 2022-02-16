6786fc2cefa040166e870925ceb54d9b41d69603
SUPRADIP DEY <97578674+DESUP2@users.noreply.github.com>
2022-02-16 09:25:08 +0530

    Add files via upload

---
+++ app/src/main/java/org/chromium/base/library_loader/NativeLibraries.java
@@ -0,0 +1,18 @@
+package org.chromium.base.library_loader;
+import org.chromium.base.annotations.SuppressFBWarnings;
+@SuppressFBWarnings
+public class NativeLibraries {
+    public static final int CPU_FAMILY_UNKNOWN = 0;
+    public static final int CPU_FAMILY_ARM = 1;
+    public static final int CPU_FAMILY_MIPS = 2;
+    public static final int CPU_FAMILY_X86 = 3;
+    public static boolean sUseLinker = true;
+    public static boolean sUseLibraryInZipFile = false;
+    public static boolean sEnableLinkerTests = false;
+    public static final String[] LIBRARIES =
+      {"chrome"};
+    static String sVersionNumber =
+      "96.0.2924.87";
+    public static int sCpuFamily =
+            CPU_FAMILY_UNKNOWN;
+}
